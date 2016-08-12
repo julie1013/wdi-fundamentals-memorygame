efa3074f8e143551c87ae8a95a3fa4880e2d742b
julie1013 <julie1380@verizon.net>
2016-08-11 20:34:31 -0400

    Conditional for comparing two cards.

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+
+if (cardOne == cardTwo){
+  alert ("You've found a match!");
+} else {
+  alert ("Sorry. Try again.");
+}

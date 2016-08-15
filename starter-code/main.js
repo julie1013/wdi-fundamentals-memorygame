36c816fb615c78cfed69d1c331db8e98c6eac856
julie1013 <julie1380@verizon.net>
2016-08-14 21:15:42 -0400

    Added event listeners. Comparing cards function is working.

--- starter-code/main.js
+++ starter-code/main.js
@@ -18,25 +18,52 @@ function createBoard(){
     cardElement.className = "card";
     cardElement.setAttribute("data-card", cards[i]);
     gameBoard.appendChild(cardElement);
-    cardElement.addEventListener("click", isTwoCards);
-    if (document.querySelector(".queen")){
-      cardElement.innerHTML = "<img src='../starter-code/images/queen.png'>";
-      cardElement.className = "queen";
+    addListeners(cardElement);
 
-    }
   }
 }
 
+// addListeners(cardElement);
 createBoard();
 
-function isMatch(){
+function isMatch(cardsInPlay){
+  for (var i = 0; i < cardsInPlay.length; i++){
+    return cardsInPlay[i] === cardsInPlay[i + 1];
+  }
 
 }
 
 function isTwoCards(){
-  cardsInPlay.push(this.getAttribute("data-card"));
+  cardsInPlay.push(this.getAttribute('data-card'));
   if (cardsInPlay.length === 2){
     isMatch(cardsInPlay);
     cardsInPlay = [];
   }
 }
+
+
+function test(){
+  alert("hi");
+}
+
+function addListeners(cardElement){
+  for (var i = 0; i < cards.length; i++){
+    cardElement.addEventListener("click", isTwoCards);
+  }
+}
+
+// function flip(cardElement){
+// cardsInPlay.push(cardElement);
+//   if (this.hasAttribute("queen")){
+//     alert("hi");
+//       cardElement.innerHTML = "<img src='../starter-code/images/queen.png'>";
+//       cardElement.className = "queen";
+//     }
+// }
+
+//clicking in the card calls functions:
+//1. flip the card
+//2. check if two cards are in play
+//3. if two cards are in play, compare them (classes, etc)
+//3a.If they are the same, player wins
+//3b.If cards aren't the same, cards are flipped over again
